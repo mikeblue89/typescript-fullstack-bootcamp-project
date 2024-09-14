80381bf7c68380172c0773ac5d08fd4c925d95a5
mikeblue89 <45115688+mikeblue89@users.noreply.github.com>
2024-09-13 21:23:51 -0600

    feat: adding variant interface

--- apps/client/src/interfaces/product.interface.ts
+++ apps/client/src/interfaces/product.interface.ts
@@ -1,8 +1,16 @@
 export interface Product {
-    id: number;
-    name: string;
-    description?: string;
-    image?: string;
-    price: number;
-  }
+  id: number;
+  name: string;
+  description?: string;
+  image?: string;
+  price: number;
+}
+
+export interface Variant {
+  id: number;
+  name: string;
+  description?: string;
+  image?: string;
+  price: number;
+}
 
